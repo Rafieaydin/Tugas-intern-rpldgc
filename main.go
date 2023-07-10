04fbc22c390f10b955dbe82e82582e1876f53444
Regyyyy <regyrahman@gmail.com>
2023-07-11 00:15:46 +0800

    Tambah nama
    Menambah nama pada Main()

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 	fmt.Println("Reinhard Efraim SItumeang/1302220001 - Hello World!")
+	fmt.Println("Regy Renanda Rahman/1302213117 - Semoga kita semua sukses terus!")
 }
