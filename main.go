c24f135d15e376969ef3473e3190400e8c78d65f
Rafieaydin <rafie.aydin1@gmail.com>
2023-07-13 17:27:38 +0700

    update nama

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello world")
 	fmt.Println("Reinhard Efraim SItumeang/1302220001 - Hello World!")
 	fmt.Println("Regy Renanda Rahman/1302213117 - Semoga kita semua sukses terus!")
+	fmt.Println("Rafie aydin ihsan/1302220065 - 1 + 1 = 2")
 }
